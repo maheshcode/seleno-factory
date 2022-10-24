e6bdd3ec33d16e49e8b09c6caf6ed3a3373a7e05
Mahesh Ganegoda <mganegoda@wiley.com>
2022-10-24 10:13:16 +0530

    format files

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -2,7 +2,6 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -11,18 +10,17 @@ public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait webDriverWait;
 
-    public void init(WebDriver driver){
+    public void init(WebDriver driver) {
         this.driver = driver;
         this.webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
         this.initFindByAnnotations(this);
     }
 
-
-    private <E extends BasePage> void initFindByAnnotations(E page){
-        PageFactory.initElements(this.driver,page);
+    private <E extends BasePage> void initFindByAnnotations(E page) {
+        PageFactory.initElements(this.driver, page);
     }
 
-    public void open(String url){
+    public void open(String url) {
         this.driver.get(url);
     }
 }
